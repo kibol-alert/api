70a4bb766f4069af268a9a027e3c6f6dee8704bd
PR <patrykreszczynski@gmail.com>
2019-10-18 11:24:31 +0200

    Empty project

---
+++ Kibol-Alert/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kibol_Alert.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
