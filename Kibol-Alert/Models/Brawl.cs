334e5d09baa4fa235a8c57a3ff506cb57c712030
PR <patrykreszczynski@gmail.com>
2019-12-10 22:49:15 +0100

    Added Brawl nad Chants

---
+++ Kibol-Alert/Models/Brawl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kibol_Alert.Models
+{
+    public class Brawl
+    {
+        public int Id { get; set; }
+        public Club FirstClub { get; set; }
+        public Club SecondClub { get; set; }
+    }
+}
