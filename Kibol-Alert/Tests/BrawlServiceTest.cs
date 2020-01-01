23e1bf7931298ba3b2b7fddcdcee3811b42401ff
PR <patrykreszczynski@gmail.com>
2020-01-01 20:55:48 +0100

    BrawlService Tests

--- Kibol-Alert/Tests/BrawlServiceTest.cs
+++ Kibol-Alert/Tests/BrawlServiceTest.cs
@@ -41,12 +41,12 @@ namespace Kibol_Alert.Tests
 
         [Theory]
         [MemberData(nameof(DataForDeletetBrawlTest))]
-        public async void DeleteBrawlTest(string FirstClubName, string SecondClubName, DateTime date, Location location, int id)
+        public async void DeleteBrawlTest(string firstClubName, string secondClubName, DateTime date, Location location, int id)
         {
             var request = new BrawlRequest()
             {
-                FirstClubName = FirstClubName,
-                SecondClubName = SecondClubName,
+                FirstClubName = firstClubName,
+                SecondClubName = secondClubName,
                 Date = date,
                 Location = location
             };
@@ -54,6 +54,51 @@ namespace Kibol_Alert.Tests
             var fakeBrawl = await _brawlService.AddBrawl(request);
             var fODBrawl = await _contextBuilder.Context.Brawls.FirstOrDefaultAsync(i => i.Id == id);
             var result = await _brawlService.DeleteBrawl(fODBrawl.Id);
+
+            Assert.True(result.Success);
+        }
+
+        [Theory]
+        [MemberData(nameof(DataForEditBrawlTest))]
+        public async void EditBrawlTest(string firstClubName, string secondClubName, DateTime date, Location location, int id, string firstClubNameEdit, string secondClubNameEdit, DateTime dateEdit, Location locationEdit)
+        {
+            var request1 = new BrawlRequest()
+            {
+                FirstClubName = firstClubName,
+                SecondClubName = secondClubName,
+                Date = date,
+                Location = location
+            };
+            var request2 = new BrawlRequest()
+            {
+                FirstClubName = firstClubNameEdit,
+                SecondClubName = secondClubNameEdit,
+                Date = dateEdit,
+                Location = locationEdit
+            };
+
+            var fakeBrawl = await _brawlService.AddBrawl(request1);
+            var fODBrawl = await _contextBuilder.Context.Brawls.FirstOrDefaultAsync(i => i.Id == id);
+            var result = await _brawlService.EditBrawl(id, request2);
+
+            Assert.True(result.Success);
+        }
+
+        [Theory]
+        [MemberData(nameof(DataForDeletetBrawlTest))]
+        public async void GetBrawlTest(string firstClubName, string secondClubName, DateTime date, Location location, int id)
+        {
+            var request = new BrawlRequest()
+            {
+                FirstClubName = firstClubName,
+                SecondClubName = secondClubName,
+                Date = date,
+                Location = location
+            };
+
+            var fakeBrawl = await _brawlService.AddBrawl(request);
+            var fODBrawl = await _contextBuilder.Context.Brawls.FirstOrDefaultAsync(i => i.Id == id);
+            var result = await _brawlService.GetBrawl(fODBrawl.Id);
 
             Assert.True(result.Success);
         }
@@ -78,6 +123,22 @@ namespace Kibol_Alert.Tests
                     new DateTime(2020,01,01,12,00,00),
                     new Location(123456.0f, 987654.0f),
                     1
+                }
+            };
+
+        public static IEnumerable<object[]> DataForEditBrawlTest =>
+            new List<object[]>
+            {
+                new object[]{
+                    "FirstClubName1",
+                    "FirstClubName2",
+                    new DateTime(2020,01,01,12,00,00),
+                    new Location(123456.0f, 987654.0f),
+                    1,
+                    "FirstClubName1Edited",
+                    "FirstClubName2Edited",
+                    new DateTime(2137,01,01,12,00,00),
+                    new Location(000000.0f, 000000.0f),
                 }
             };
     }
